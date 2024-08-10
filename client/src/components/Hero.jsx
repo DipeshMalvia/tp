411e590371d48f72142919ad4ecfa704a9a7c706
Akash Pawar <akashgp1643pawar+git@gmail.com>
2024-08-11 00:46:36 +0530

    version 1

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+
+export default function Hero() {
+    return (
+        <section className="bg-orange-200">
+            <div className="container mx-auto flex flex-col lg:flex-row items-center">
+                {/* <div className="lg:w-1/2">
+                    <h1 className="text-4xl font-bold mb-4">Lipocontrast</h1>
+                    <p className="mb-4">The ultimate single session fat freeze technology.</p>
+                    <button className="bg-black text-white px-4 py-2 rounded">Book an Appointment</button>
+                </div>
+                <div className="lg:w-1/2">
+                </div> */}
+                <img src="/lipocontrast.jpg" width={"100%"} alt="Lipocontrast Device" />
+            </div>
+        </section>
+    )
+}
