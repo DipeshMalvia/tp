32eeaea977b7ea689038be20351eaf38efc7f738
Akash Pawar <akashgp1643pawar+git@gmail.com>
2024-08-11 01:28:22 +0530

    UI fix

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,16 +1,35 @@
+import { MdLocationOn } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Hero() {
     return (
         <section className="bg-orange-200">
-            <div className="container mx-auto flex flex-col lg:flex-row items-center">
-                {/* <div className="lg:w-1/2">
-                    <h1 className="text-4xl font-bold mb-4">Lipocontrast</h1>
-                    <p className="mb-4">The ultimate single session fat freeze technology.</p>
-                    <button className="bg-black text-white px-4 py-2 rounded">Book an Appointment</button>
-                </div>
-                <div className="lg:w-1/2">
-                </div> */}
+            <div className="container mx-auto flex flex-col lg:flex-col items-center relative">
                 <img src="/lipocontrast.jpg" width={"100%"} alt="Lipocontrast Device" />
+
+                <div className="hidden sm:flex sm:p-2 xl:p-6 flex-col absolute bottom-0 left-0 ">
+                    <div className="flex flex-row gap-1 items-center">
+                        <div><MdLocationOn className="text-xl"/></div>
+                        <div className="flex flex-col text-sm">
+                            <div>115 B Mittal Court,</div>
+                            <div>Nariman Point, Mumbai 400 021</div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row items-center gap-2">
+                        <div><FaPhoneAlt /></div>
+                        <div>+91 2222843000 | +91 2222825555</div>
+                    </div>
+                    <div className="flex gap-2 text-xl mt-2">
+                        <FaFacebookSquare />
+                        <FaInstagramSquare />
+                        <FaSquareXTwitter />
+                        <FaLinkedin />
+                    </div>
+                </div>
             </div>
         </section>
     )
